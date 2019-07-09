af832edbee062e655ae166d679f2e819a2d3d81d
BALURAAM <52704338+BALURAAM@users.noreply.github.com>
2019-07-09 18:06:40 +0530

    Update one.c

--- one.c
+++ one.c
@@ -3,8 +3,13 @@ void main()
 {
   int a;
   scanf("%d",&a);
-  if(a%2==0)
-    printf("Even");
-  else if(a%2!=0)
-    printf("Odd");
+  if(a>0)
+  {
+    if(a%2==0)
+      printf("Even");
+    else if(a%2!=0)
+      printf("Odd");
+  }
+  else
+    printf("Invalid");
 }
