b57c863ffece00049227179b0f7b2536268a8c92
BALURAAM <52704338+BALURAAM@users.noreply.github.com>
2019-07-09 18:03:33 +0530

    Update one.c

--- one.c
+++ one.c
@@ -3,8 +3,8 @@ void main()
 {
   int a;
   scanf("%d",&a);
-  if(a>0)
-    printf("Positive");
-  else if(a<0)
-    printf("Negative");
+  if(a%2==0)
+    printf("Even");
+  else if(a%2!=0)
+    printf("Odd");
 }
